2cda062abaa4ae0a1cb058e4e86ad2fb32329ec1
Ockrassa Morales <a.ockrassa.morales@accenture.com>
2017-07-12 00:38:57 -0400

    update

--- CodeBook.md
+++ CodeBook.md
@@ -6,79 +6,20 @@ Variables
 
 Variable                                   | Comments
 -------------------------------------------|-----------
-act_labels                                 |  name of activity subject (LAYING,SITTING,STANDING,WALKING,WALKING_DOWNSTAIRS,WALKING_UPSTAIRS)
 train.subject                              |  subject identifier for training set data (1-30)
 train.x					                   |  x values of training set
 train.y					                   |  y values of training set
-train.accx				                   |  values of BodyAcc-X training set
-train.accy                 				   |  values of BodyAcc-Y training set
-train.accz               				   |  values of BodyAcc-Z training set
-train.gyrox            					   |  values of GravityAcc-X training set
-train.gyroy            					   |  values of GravityAcc-Y training set
-train.gyroz            					   |  values of GravityAcc-Z training set
-train.totalx            				   |  values of TotalAcc-X training set
-train.totaly            				   |  values of TotalAcc-Y training set
-train.totalz            				   |  values of TotalAcc-Z training set
-train.actlabels              			   |  merge activity label with training data
-train.subject$type_data        			   |  type of data (train or test)
-train.subject$mean			               |  mean of X -mean()-training set
-train.subject$sds			               |  standart deviation of X-training set
-train.subject$act			               |  merge activity to training data
-train.subject$mean_body_acc_x              |  mean of tBodyAcc-mean() X training set
-train.subject$sds_body_acc_x               |  standard deviation of tBodyAccJerk-sds()-X training set
-train.subject$mean_body_acc_y              |  mean of tBodyAcc-mean() Y training set
-train.subject$sds_body_acc_y               |  standard deviation of tBodyAccJerk-sds()-Y training set
-train.subject$mean_body_acc_z              |  mean of tBodyAcc-mean() Z training set
-train.subject$sds_body_acc_z               |  standard deviation of tBodyAccJerk-sds()-Z training set
-train.subject$mean_gyro_x                  |  mean of GyroAcc-mean() X training set
-train.subject$sds_gyro_x                   |  standard deviation of GyroAccJerk-sds()-X training set
-train.subject$mean_gyro_y                  |  mean of GyroAcc-mean() Y training set
-train.subject$sds_gyro_y                   |  standard deviation of GyroAccJerk-sds()-Y training set
-train.subject$mean_gyro_z                  |  mean of GyroAcc-mean() Z training set
-train.subject$sds_gyro_z                   |  standard deviation of GyroAccJerk-sds()-Z training set
-train.subject$mean_total_x                 |  mean of TotalAcc-mean() X training set
-train.subject$sds_total_x                  |  standard deviation of TotalAccJerk-sds()-X training set
-train.subject$mean_total_y                 |  mean of TotalAcc-mean() Y training set
-train.subject$sds_total_y                  |  standard deviation of TotalAccJerk-sds()-Y training set
-train.subject$mean_total_<                 |  mean of TotalAcc-mean() Z training set
-train.subject$sds_total_<                  |  standard deviation of TotalAccJerk-sds()-Z training set
 test.subject                               |  subject identifier for testing set data (1-30)
 test.x					                   |  x values of testing set
 test.y					                   |  y values of testing set
-test.accx				                   |  values of BodyAcc-X testing set
-test.accy                 				   |  values of BodyAcc-Y testing set
-test.accz               				   |  values of BodyAcc-Z testing set
-test.gyrox            					   |  values of GravityAcc-X testing set
-test.gyroy            					   |  values of GravityAcc-Y testing set
-test.gyroz            					   |  values of GravityAcc-Z testing set
-test.totalx            				       |  values of TotalAcc-X testing set
-test.totaly            				       |  values of TotalAcc-Y testing set
-test.totalz            				       |  values of TotalAcc-Z testing set
-test.actlabels              			   |  merge activity label with testing data
-test.subject$type_data        			   |  type of data (test or test)
-test.subject$mean			               |  mean of X -mean()-testing set
-test.subject$sds			               |  standart deviation of X-testing set
-test.subject$act			               |  merge activity to testing data
-test.subject$mean_body_acc_x               |  mean of tBodyAcc-mean() X testing set
-test.subject$sds_body_acc_x                |  standard deviation of tBodyAccJerk-sds()-X testing set
-test.subject$mean_body_acc_y               |  mean of tBodyAcc-mean() Y testing set
-test.subject$sds_body_acc_y                |  standard deviation of tBodyAccJerk-sds()-Y testing set
-test.subject$mean_body_acc_z               |  mean of tBodyAcc-mean() Z testing set
-test.subject$sds_body_acc_z                |  standard deviation of tBodyAccJerk-sds()-Z testing set
-test.subject$mean_gyro_x                   |  mean of GyroAcc-mean() X testing set
-test.subject$sds_gyro_x                    |  standard deviation of GyroAccJerk-sds()-X testing set
-test.subject$mean_gyro_y                   |  mean of GyroAcc-mean() Y testing set
-test.subject$sds_gyro_y                    |  standard deviation of GyroAccJerk-sds()-Y testing set
-test.subject$mean_gyro_z                   |  mean of GyroAcc-mean() Z testing set
-test.subject$sds_gyro_z                    |  standard deviation of GyroAccJerk-sds()-Z testing set
-test.subject$mean_total_x                  |  mean of TotalAcc-mean() X testing set
-test.subject$sds_total_x                   |  standard deviation of TotalAccJerk-sds()-X testing set
-test.subject$mean_total_y                  |  mean of TotalAcc-mean() Y testing set
-test.subject$sds_total_y                   |  standard deviation of TotalAccJerk-sds()-Y testing set
-test.subject$mean_total_<                  |  mean of TotalAcc-mean() Z testing set
-test.subject$sds_total_<                   |  standard deviation of TotalAccJerk-sds()-Z testing set
-data_set				                   |  data set with merge of training and test set, with average and standard deviation of each measurement
-data_summ				                   |  tidy data set with the average of each variable for each activity and each subject.
+data_set.x				                   |  data set with merge x of training and test data
+data_set.y				                   |  data set with merge y of training and test data
+data_set.subject		                   |  data set with merge subject of training and test data
+features				                   |  features name of x measurement
+features_mean_std		                   |  features name of x measurement, with only mean and standard deviation
+act_labels                                 |  name of activity subject (LAYING,SITTING,STANDING,WALKING,WALKING_DOWNSTAIRS,WALKING_UPSTAIRS)
+all_data				                   |  tidy data set with the average for each measurement.
+tidy_data				                   |  tidy data set with the average of each variable for each activity and each subject.
 
 
 Transformations
--- run_analysis.R
+++ run_analysis.R
@@ -1,93 +1,47 @@
-library(matrixStats)
-act_labels <- read.table("data/UCI HAR Dataset/activity_labels.txt")
+library(plyr)
+
+# Step 1
+# Merge the training and test sets to create one data set
+###############################################################################
 
 #read of training data
 train.subject <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
 train.x <- read.table("data/UCI HAR Dataset/train/X_train.txt")
 train.y <- read.table("data/UCI HAR Dataset/train/y_train.txt")
-train.accx <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-train.accy <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-train.accz <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-train.gyrox <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-train.gyroy <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-train.gyroz <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-train.totalx <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-train.totaly <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-train.totalz <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
-
-#merge of training data
-train.actlabels <- merge(train.y,act_labels,by.x = "V1",by.y = "V1", sort = FALSE)
-train.subject$type_data = "train" 
-train.subject$mean = rowMeans(train.x,na.rm = TRUE)
-train.subject$sds = rowSds(as.matrix(train.x),na.rm = TRUE)
-train.subject$act = train.actlabels$V2
-train.subject$mean_body_acc_x = rowMeans(train.accx,na.rm = TRUE)
-train.subject$sds_body_acc_x = rowSds(as.matrix(train.accx),na.rm = TRUE)
-train.subject$mean_body_acc_y = rowMeans(train.accy,na.rm = TRUE)
-train.subject$sds_body_acc_y = rowSds(as.matrix(train.accx),na.rm = TRUE)
-train.subject$mean_body_acc_z = rowMeans(train.accz,na.rm = TRUE)
-train.subject$sds_body_acc_z = rowSds(as.matrix(train.accx),na.rm = TRUE)
-train.subject$mean_gyro_x = rowMeans(train.gyrox,na.rm = TRUE)
-train.subject$sds_gyro_x = rowSds(as.matrix(train.gyrox),na.rm = TRUE)
-train.subject$mean_gyro_y = rowMeans(train.gyroy,na.rm = TRUE)
-train.subject$sds_gyro_y = rowSds(as.matrix(train.gyroy),na.rm = TRUE)
-train.subject$mean_gyro_z = rowMeans(train.gyroz,na.rm = TRUE)
-train.subject$sds_gyro_z = rowSds(as.matrix(train.gyroz),na.rm = TRUE)
-train.subject$mean_total_x = rowMeans(train.totalx,na.rm = TRUE)
-train.subject$sds_total_x = rowSds(as.matrix(train.totalx),na.rm = TRUE)
-train.subject$mean_total_y = rowMeans(train.totaly,na.rm = TRUE)
-train.subject$sds_total_y = rowSds(as.matrix(train.totaly),na.rm = TRUE)
-train.subject$mean_total_z = rowMeans(train.totalz,na.rm = TRUE)
-train.subject$sds_total_z = rowSds(as.matrix(train.totalz),na.rm = TRUE)
 
 #read of testing data
 test.subject <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
 test.x <- read.table("data/UCI HAR Dataset/test/X_test.txt")
 test.y <- read.table("data/UCI HAR Dataset/test/y_test.txt")
-test.accx <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
-test.accy <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt")
-test.accz <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt")
-test.gyrox <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt")
-test.gyroy <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt")
-test.gyroz <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt")
-test.totalx <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt")
-test.totaly <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
-test.totalz <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
 
-#merge of test data
-test.actlabels <- merge(test.y,act_labels,by.x = "V1",by.y = "V1", sort = FALSE)
-test.subject$type_data = "test" 
-test.subject$mean = rowMeans(test.x,na.rm = TRUE)
-test.subject$sds = rowSds(as.matrix(test.x),na.rm = TRUE)
-test.subject$act = test.actlabels$V2
-test.subject$mean_body_acc_x = rowMeans(test.accx,na.rm = TRUE)
-test.subject$sds_body_acc_x = rowSds(as.matrix(test.accx),na.rm = TRUE)
-test.subject$mean_body_acc_y = rowMeans(test.accy,na.rm = TRUE)
-test.subject$sds_body_acc_y = rowSds(as.matrix(test.accx),na.rm = TRUE)
-test.subject$mean_body_acc_z = rowMeans(test.accz,na.rm = TRUE)
-test.subject$sds_body_acc_z = rowSds(as.matrix(test.accx),na.rm = TRUE)
-test.subject$mean_gyro_x = rowMeans(test.gyrox,na.rm = TRUE)
-test.subject$sds_gyro_x = rowSds(as.matrix(test.gyrox),na.rm = TRUE)
-test.subject$mean_gyro_y = rowMeans(test.gyroy,na.rm = TRUE)
-test.subject$sds_gyro_y = rowSds(as.matrix(test.gyroy),na.rm = TRUE)
-test.subject$mean_gyro_z = rowMeans(test.gyroz,na.rm = TRUE)
-test.subject$sds_gyro_z = rowSds(as.matrix(test.gyroz),na.rm = TRUE)
-test.subject$mean_total_x = rowMeans(test.totalx,na.rm = TRUE)
-test.subject$sds_total_x = rowSds(as.matrix(test.totalx),na.rm = TRUE)
-test.subject$mean_total_y = rowMeans(test.totaly,na.rm = TRUE)
-test.subject$sds_total_y = rowSds(as.matrix(test.totaly),na.rm = TRUE)
-test.subject$mean_total_z = rowMeans(test.totalz,na.rm = TRUE)
-test.subject$sds_total_z = rowSds(as.matrix(test.totalz),na.rm = TRUE)
+# merge training and test data
+data_set.x <- rbind(train.x, test.x)
+data_set.y <- rbind(train.y, test.y)
+data_set.subject <- rbind(train.subject, test.subject)
 
-#merge training and test set
-data_set <- rbind(train.subject, test.subject)
-colnames(data_set)[1] = "id"
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement
+###############################################################################
+features <- read.table("data/UCI HAR Dataset/features.txt")
+features_mean_std <- grep("(mean|std)", features$V2)
+data_set.x <- data_set.x[, features_mean_std]
+names(data_set.x) <- features[features_mean_std, 2]
 
-#tidy data set with the average of each variable for each activity and each subject.
-data_summ <- select(data_set, id,act, mean) %>%
-  group_by(id,act) %>%
-  summarize(mean = mean(mean)) %>%
-  spread(act,mean)
+# Step 3
+# Use descriptive activity names to name the activities in the data set
+###############################################################################
+act_labels <- read.table("data/UCI HAR Dataset/activity_labels.txt")
+data_set.y$V1 <- act_labels[data_set.y$V1, 2]
 
-write.table(data_summ,file = "tidy_wereable_summary", row.name = FALSE)
-data_summ
+# Step 4
+# Appropriately label the data set with descriptive variable names
+###############################################################################
+names(data_set.y) <- "activity"
+names(data_set.subject) <- "subject"
+all_data <- cbind(data_set.x, data_set.y, data_set.subject)
+
+# Step 5
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject
+###############################################################################
+tidy_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:79]))
+write.table(tidy_data, "tidy_data.txt", row.name = FALSE)
